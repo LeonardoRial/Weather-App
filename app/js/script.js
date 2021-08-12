73219752f3b51a32361d7581bacdb726f1e1d475
LeonardoCamposRial <73833933+LeonardoCamposRial@users.noreply.github.com>
2021-08-12 13:24:30 +0100

    WeatherApp

--- app/js/script.js
+++ app/js/script.js
@@ -5,8 +5,9 @@ const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 const weather = document.querySelector(".weather");
 const icon = document.querySelector(".icon");
+const info = document.querySelector("body");
 
-btn.addEventListener("click", () => {
+function getWeather() {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=374fbf091349541b2aefe15b63576863`
   )
@@ -16,10 +17,19 @@ btn.addEventListener("click", () => {
       city.innerHTML = `${data.name}, ${data.sys.country}`;
       temp.innerHTML = `Temperature: ${celsius.toFixed(1)}` + "&#8451";
       humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-      weather.innerHTML =
-        data.weather[0].description[0].toUpperCase() +
-        data.weather[0].description.slice(1);
+      weather.innerHTML = data.weather[0].description;
       icon.style.backgroundImage = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}.png)`;
+      info.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${data.weather[0].main})`;
+
       console.log(data);
     });
+}
+
+btn.addEventListener("click", () => {
+  getWeather();
+});
+input.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    getWeather();
+  }
 });
