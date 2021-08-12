9f7f4c57bc84bb37b42fdf188d8222cde62bf63f
LeonardoCamposRial <73833933+LeonardoCamposRial@users.noreply.github.com>
2021-08-12 10:56:58 +0100

    WeatherApp

--- app/js/script.js
+++ app/js/script.js
@@ -4,6 +4,7 @@ const city = document.querySelector(".city");
 const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 const weather = document.querySelector(".weather");
+const icon = document.querySelector(".icon");
 
 btn.addEventListener("click", () => {
   fetch(
@@ -12,12 +13,13 @@ btn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       const celsius = data.main.temp - 273.15;
-      city.innerHTML = data.name;
+      city.innerHTML = `${data.name}, ${data.sys.country}`;
       temp.innerHTML = `Temperature: ${celsius.toFixed(1)}` + "&#8451";
       humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
       weather.innerHTML =
         data.weather[0].description[0].toUpperCase() +
         data.weather[0].description.slice(1);
+      icon.style.backgroundImage = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}.png)`;
       console.log(data);
     });
 });
